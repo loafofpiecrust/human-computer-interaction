cc3195d951cda5acd57625b4bcbcc3989a0f7e4d
Taylor Snead <taylorsnead@gmail.com>
2020-10-22 11:02:14 -0400

    Add basic index page
    Setup gatsby and typescript, ready for deploying on Netlify

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteMetadata: {
+    title: "HCI",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Human Computer Interaction`,
+        short_name: `HCI`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    "gatsby-plugin-offline",
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-typescript-checker",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+  ],
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default () => (
+  <main>
+    <h1>Human Computer Interaction</h1>
+    <section>
+      <h2>Authors</h2>
+      <ul>
+        <li>Taylor Snead</li>
+        <li>Jada Letts</li>
+      </ul>
+    </section>
+  </main>
+)
