81c95dd1460d909a528f6ae0ccd44b2b0f4606e5
Taylor Snead <taylorsnead@gmail.com>
2020-10-29 18:44:30 -0400

    :sparkles: Add requirement analysis PDF

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,17 @@ import React from "react"
 
 export default () => (
   <main>
-    <h1>Human Computer Interaction</h1>
+    <h1>Personalized Music Timeer</h1>
     <section>
       <h2>Authors</h2>
       <ul>
         <li>Taylor Snead</li>
         <li>Jada Letts</li>
       </ul>
+    </section>
+    <section>
+      <h2>Requirement Analysis</h2>
+      <a href="/requirement-analysis.pdf">Download PDF</a>
     </section>
   </main>
 )
