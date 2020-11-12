c4c5fa65498df341fbd8f95922857ac23634a4aa
Taylor Snead <taylorsnead@gmail.com>
2020-11-12 17:57:19 -0500

    :sparkles: Add links to prototype slides

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,5 +14,27 @@ export default () => (
       <h2>Requirement Analysis</h2>
       <a href="/requirement-analysis.pdf">Download PDF</a>
     </section>
+    <section>
+      <h2>Prototype Version 1</h2>
+      <p>
+        We drafted our first prototype on paper, then translated that prototype
+        into a Google Slides document with some interactivity. If you click
+        "Present," you can click the buttons on each screen to navigate around
+        the interface
+      </p>
+      <a href="https://docs.google.com/presentation/d/14BN51xecmJ5L1wbAWgIBcUD4nZO7_D8wTgoHc6qnwhs/edit?usp=sharing">
+        View Google Slides
+      </a>
+    </section>
+    <section>
+      <h2>Prototype Version 2</h2>
+      <p>
+        Based on the feedback that we received from our usability testing, we
+        revised the prototype within Google Slides
+      </p>
+      <a href="https://docs.google.com/presentation/d/1MYrczgZWEjlRhtHVc-dgmNAizb4WE9VR5nP_wZ0nsJI/edit?usp=sharing">
+        View Google Slides
+      </a>
+    </section>
   </main>
 )
