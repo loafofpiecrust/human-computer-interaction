ad2efc1bc4b566fe72e1676f14a1d591bea30b9b
JadaDLetts <jletts77@gmail.com>
2020-11-18 13:30:41 -0500

    new page timer created

---
+++ src/pages/timer.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import '../style/timer.css'
+import "bootstrap/dist/css/bootstrap.css"
+import "@fortawesome/free-solid-svg-icons"
+import "@fortawesome/react-fontawesome"
+
+export default () => (
+  <main>
+    <h1>Personalized Music Timer</h1>
+
+    <section className={"ttl-btn"}>
+      <div className={"row"}>
+        <h2 className={"col-8"}>Your Timers</h2>
+        <button className={"button primary col-2"}>+</button>
+      </div>
+    </section>
+    <br/>
+    <section className={"box"}>
+
+    </section>
+
+  </main>
+)
